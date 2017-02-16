237e430d82aeee116cda76252ec68da1f1a09563
Devin McPherson <dmcpatrick44@aol.com>
2017-02-16 00:04:22 -0500

    React Native Setup Complete

---
+++ albums/index.android.js
@@ -0,0 +1,14 @@
+// Import a library to help create a component
+import React from 'react';
+import {
+  Text,
+  AppRegistry
+ } from 'react-native';
+
+// Create a component
+const App = () => (
+    <Text>Devin McPherson</Text>
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
